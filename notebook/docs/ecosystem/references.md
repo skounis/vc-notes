687c8625a0ea37cade711eb06462147fe56c8805
Stavros Kounis <skounis@gmail.com>
2022-04-15 13:52:08 +0200

    Move documentation under /notebook

---
+++ notebook/docs/ecosystem/references.md
@@ -0,0 +1,53 @@
+---
+sidebar_position: 1
+---
+
+# References
+
+## W3C
+* [Verifiable Credentials Data Model v1.1](https://www.w3.org/TR/vc-data-model/)
+* [Decentralized Identifiers (DIDs) v1.0](https://www.w3.org/TR/did-core/)
+
+
+## [Veramo](https://veramo.io)
+
+> Performant and modular APIs for Verifiable Data and SSI.
+
+Create and manage decentralized identifiers + verifiable credentials without worrying about interop and vendor lock-in.
+
+```bash
+npm i @veramo/cli -g
+```
+
+### TODO
+1. [ ] [Setup and Deploy an Agent on Heroku](https://veramo.io/docs/dev_tools/agent_explorer) 
+2. [ ] [Explore the agent](https://explore.veramo.dev)
+
+## [Identity](https://www.identity.com)
+
+> Decentralized Identity Verification
+
+[Identity.com](https://www.identity.com) is an open source ecosystem providing access to on-demand, secure identity verification. Governed by a staking mechanism designed to ensure compliance and good behavior within the ecosystem, Identity.com grants users, requesters, and validators around the world access to low-cost reusable identity verification powered by CVC tokens.
+
+## [Decentralized-id](https://decentralized-id.com/)
+> Decentralized Identity Web Directory
+
+Meant to provide a high-level view of the Self Sovereign Decentralized Identity landscape.
+
+* [Verifiable Credentials - Adoption](https://decentralized-id.com/web-standards/w3c/wg/vc/verifiable-credentials/adoption/)
+
+## [Evernym](https://www.evernym.com/)
+> The world’s leading platform for verifiable credentials
+
+From the creators of Hyperledger Indy and Sovrin
+
+Build and deploy self-sovereign identity solutions, with the technology and go-to-market resources powering the largest implementations of digital credentials in production.
+
+## Libraries
+* [Verifiable Credentials JS Library (@digitalbazaar/vc)](https://github.com/digitalbazaar/vc-js)
+* [did-jwt-vc](https://github.com/decentralized-identity/did-jwt-vc) - Create and verify W3C Verifiable Credentials and Presentations in JWT format
+* [Verifiable Credential Library](https://www.npmjs.com/package/@cef-ebsi/verifiable-credential) - Create and verify EBSI-compliant W3C Verifiable Credentials in JWT format.
+8 [Identity.com Verifiable Credential Library](https://www.npmjs.com/package/@identity.com/credential-commons) - Javascript Library provides functionality around Verifiable Credentials (VC) `WIP`
+## Articles
+
+* [Verifiable Credentials The Ultimate Beginners Guide!](https://tykn.tech/verifiable-credentials/)
---
+++ notebook/docusaurus.config.js
@@ -0,0 +1,119 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'VC Notes',
+  tagline: 'W3C Verifiable Credentials',
+  url: 'https://vc-notes.github.io',
+  baseUrl: '/vc-notes/build/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'skounis', // Usually your GitHub org/user name.
+  projectName: 'vc-notes', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/skounis/vc-notes/blob/main/docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/skounis/vc-notes/blob/main/docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'VC Notes',
+        logo: {
+          alt: 'My Site Logo',
+          // src: 'img/logo.svg',
+          src: 'img/keys-svgrepo-com.svg'
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'welcome',
+            position: 'left',
+            label: 'Welcome',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/skounis/vc-notes',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Welcome',
+                to: '/docs/welcome',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              // {
+              //   label: 'Stack Overflow',
+              //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              // },
+              // {
+              //   label: 'Discord',
+              //   href: 'https://discordapp.com/invite/docusaurus',
+              // },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/skounis',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/skounis/vc-notes',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Stavros Kounis. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
