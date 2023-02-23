a1f0875351a952392db1d8d7b2d1f93710bbfd77
Stavros Kounis <skounis@gmail.com>
2023-02-23 08:27:25 +0100

    Update docusaurus.config.js

--- notebook/docusaurus.config.js
+++ notebook/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/skounis/vc-notes/blob/main/docs/',
+          editUrl: 'https://github.com/skounis/vc-notes/blob/main/notebook/',
         },
         blog: {
           showReadingTime: true,
